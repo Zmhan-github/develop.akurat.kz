e507e89cf416a8741313b7115976223a36058efb
Marat <regeon-93@inbox.ru>
2020-05-20 19:47:30 +0500

    update config migrations

--- sequelize/config.js
+++ sequelize/config.js
@@ -6,10 +6,18 @@ dotenv.config({ path: path.resolve(__dirname, '../.env') });
 module.exports.development = {
   dialect: 'mysql',
   seederStorage: 'sequelize',
-  url: process.env.DB_URL
+  database: process.env.DB_NAME,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASS,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
 };
 
 module.exports.production = {
   dialect: 'mysql',
-  url: process.env.DB_URL
+  database: process.env.DB_NAME,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASS,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
 };
