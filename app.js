6974ae387d1e0aa66c24cd52127b176d872f374f
Marat <regeon-93@inbox.ru>
2020-05-20 06:39:13 +0500

    Update(app.js): mistake line:4

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 http.createServer(function(request, response) {
-  response.writeHead(200, {'Content-Type': 'aplication/json'});
+  response.writeHead(200, {'Content-Type': 'application/json'});
   response.end(process.env);
 }).listen(process.env.PORT);
 
