a0865c11929769ce0ff78738948773735940d0e5
Marat <regeon-93@inbox.ru>
2020-05-20 06:25:43 +0500

    adding simple server

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("Init")
+const http = require('http');
+
+http.createServer(function(request, response) {
+  response.writeHead(200, {'Content-Type': 'aplication/json'});
+  response.end(process.env);
+}).listen(process.env.PORT);
+
+console.log('App is running...');
